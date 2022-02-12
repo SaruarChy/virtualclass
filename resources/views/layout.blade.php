4ad500628ee4ab66a5752faacf9dbff565c7f6c2
debobroto <devdb017@gmail.com>
2022-02-13 00:27:35 +0600

    bootstrap added

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('js/app.js') }}" rel="stylesheet">
+    <title>p400</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
